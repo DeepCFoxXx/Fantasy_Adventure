c9ac34b372b7219b2c904ec11aecd59d8fefc434
DeepCFoxXx <learncode23@gmail.com>
2018-05-09 12:45:37 +0100

    added fighter subclass

---
+++ Fantasy_Time/src/main/java/Enemies/Fighters/Fighter.java
@@ -0,0 +1,11 @@
+package Enemies.Fighters;
+
+import Enemies.Enemy;
+
+public abstract class Fighter extends Enemy {
+
+    public Fighter(String name, int healthValue) {
+        super(name, healthValue);
+    }
+
+}
