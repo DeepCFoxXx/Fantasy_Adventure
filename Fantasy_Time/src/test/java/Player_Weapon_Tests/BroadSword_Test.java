f99fe2a9e42393664c83074c0aef3680201627de
DeepCFoxXx <learncode23@gmail.com>
2018-05-09 14:15:50 +0100

    broad sword passing

---
+++ Fantasy_Time/src/test/java/Player_Weapon_Tests/BroadSword_Test.java
@@ -0,0 +1,29 @@
+package Player_Weapon_Tests;
+
+import Enemies.Enemy;
+import Enemies.Fighters.Orc;
+import Players.Warriors.Weapons.BroadSword;
+import Players.Warriors.Weapons.Trident;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BroadSword_Test {
+
+    BroadSword broadSword;
+    Enemy enemy;
+
+    @Before
+    public void before() {
+        broadSword = new BroadSword();
+        enemy = new Orc("Uzul", 1000);
+    }
+
+    @Test
+    public void canAttack() {
+        broadSword.attack(enemy);
+        assertEquals(875, enemy.getHealthValue());
+    }
+
+}
