93d92d3fe95558382dbe110c4b8b9997e21759f2
DeepCFoxXx <learncode23@gmail.com>
2018-05-09 15:09:05 +0100

    warhammer passing

---
+++ Fantasy_Time/src/test/java/Enemy_Weapon_Tests/Warhammer_Test.java
@@ -0,0 +1,42 @@
+package Enemy_Weapon_Tests;
+
+import Enemies.Fighters.Weapons.IWeaponEnemy;
+import Enemies.Fighters.Weapons.WarHammer;
+import Players.Player;
+import Players.Warriors.Barbarian;
+import Players.Warriors.Weapons.IWeapon;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Warhammer_Test {
+
+    WarHammer warHammer;
+    Player barbarian;
+    IWeaponEnemy warhammer;
+    IWeapon greatSword;
+
+    @Before
+    public void before() {
+        warHammer = new WarHammer();
+        barbarian = new Barbarian("Conan", 600, greatSword);
+    }
+
+    @Test
+    public void canDamage() {
+        warHammer.attack(barbarian);
+        assertEquals(400, barbarian.getHealthValue());
+    }
+
+    @Test
+    public void PlayerHasName() {
+        assertEquals("Conan", barbarian.getName());
+    }
+
+    @Test
+    public void PlayerHasHealthValue() {
+        assertEquals(600, barbarian.getHealthValue());
+    }
+
+}
