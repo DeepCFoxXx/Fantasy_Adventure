6e51fcea17559d5d3b372ae8cd34e4ce3b5a3f08
DeepCFoxXx <learncode23@gmail.com>
2018-05-09 15:16:54 +0100

    last enemy weapon test passing

---
+++ Fantasy_Time/src/test/java/Enemy_Weapon_Tests/BattleAxe_Test.java
@@ -0,0 +1,42 @@
+package Enemy_Weapon_Tests;
+
+import Enemies.Fighters.Weapons.BattleAxe;
+import Enemies.Fighters.Weapons.OrcishSword;
+import Players.Player;
+import Players.Warriors.Barbarian;
+import Players.Warriors.Weapons.IWeapon;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BattleAxe_Test {
+
+    BattleAxe battleAxe;
+    Player barbarian;
+    IWeapon greatSword;
+
+    @Before
+    public void before() {
+        battleAxe = new BattleAxe();
+        barbarian = new Barbarian("Conan", 600, greatSword);
+    }
+
+    @Test
+    public void canDamage() {
+        battleAxe.attack(barbarian);
+        assertEquals(420, barbarian.getHealthValue());
+    }
+
+    @Test
+    public void PlayerHasName() {
+        assertEquals("Conan", barbarian.getName());
+    }
+
+    @Test
+    public void PlayerHasHealthValue() {
+        assertEquals(600, barbarian.getHealthValue());
+    }
+
+
+}
