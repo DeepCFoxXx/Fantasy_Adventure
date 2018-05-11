40f94ac85a60c00175364092b42e397a0360c726
DeepCFoxXx <learncode23@gmail.com>
2018-05-11 12:08:17 +0100

    orc take damage passing

--- Fantasy_Time/src/test/java/Enemy_Tests/Orc_Test.java
+++ Fantasy_Time/src/test/java/Enemy_Tests/Orc_Test.java
@@ -2,6 +2,11 @@ package Enemy_Tests;
 
 import Enemies.Fighters.Orc;
 import Enemies.Fighters.Weapons.IWeaponEnemy;
+import Enemies.Fighters.Weapons.OrcishSword;
+import Players.Player;
+import Players.Warriors.Barbarian;
+import Players.Warriors.Weapons.GreatSword;
+import Players.Warriors.Weapons.IWeapon;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -11,10 +16,16 @@ public class Orc_Test {
 
     Orc orc;
     IWeaponEnemy warhammer;
+    IWeaponEnemy orcishSword;
+    Player player;
+    IWeapon greatsword;
 
     @Before
     public void before() {
         orc = new Orc("Uzul", 1000, warhammer, 100);
+        orcishSword = new OrcishSword();
+        player = new Barbarian("Conan", 600, greatsword, 80);
+        greatsword = new GreatSword();
     }
 
     @Test
@@ -30,6 +41,12 @@ public class Orc_Test {
     @Test
     public void hasPhysicalResistance() {
         assertEquals(100, orc.getPhysicalResistance());
+    }
+
+    @Test
+    public void canTakeDamage() {
+        orc.takeDamage(250);
+        assertEquals(750, orc.getHealthValue());
     }
 
 }
