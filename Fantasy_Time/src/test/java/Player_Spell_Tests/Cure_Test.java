6e5225e8a63f3c5aa17acea4a12526bfa2b07725
DeepCFoxXx <learncode23@gmail.com>
2018-05-17 12:31:15 +0100

    increase health with cure passing

---
+++ Fantasy_Time/src/test/java/Player_Spell_Tests/Cure_Test.java
@@ -0,0 +1,33 @@
+package Player_Spell_Tests;
+
+import Players.MagicalUsers.Healing_Spells.Cure;
+import Players.Player;
+import Players.Warriors.Barbarian;
+import Players.Warriors.Weapons.GreatSword;
+import Players.Warriors.Weapons.IWeapon;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Cure_Test {
+
+    Cure cure;
+    Player barbarian;
+    IWeapon greatSword;
+
+    @Before
+    public void before() {
+        cure = new Cure();
+        barbarian = new Barbarian("Conan", 600, 80, 10, 50, 60, 20, false, greatSword, 100);
+        greatSword = new GreatSword();
+    }
+
+    @Test
+    public void canIncreaseHealth() {
+        barbarian.takeDamage(500);
+        cure.heal(barbarian);
+        assertEquals(300, barbarian.getHealthValue());
+    }
+
+}
