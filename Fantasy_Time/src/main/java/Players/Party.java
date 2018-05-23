2e312df422dee4a8e9457f5cf6814069cb2a054a
DeepCFoxXx <learncode23@gmail.com>
2018-05-23 16:06:02 +0100

    party starts empty passing

---
+++ Fantasy_Time/src/main/java/Players/Party.java
@@ -0,0 +1,16 @@
+package Players;
+
+import java.util.ArrayList;
+
+public class Party {
+
+    ArrayList<Player>members;
+
+    public Party() {
+        this.members = new ArrayList<Player>();
+    }
+
+    public int getMembersCount() {
+        return this.members.size();
+    }
+}
