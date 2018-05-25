9e3b370cae82aee47acc0bfc0587558c442dbba8
DeepCFoxXx <learncode23@gmail.com>
2018-05-25 12:59:26 +0100

    added party class

---
+++ Fantasy_Time/src/main/java/Players/Party.java
@@ -0,0 +1,13 @@
+package Players;
+
+import java.util.ArrayList;
+
+public class Party {
+
+    ArrayList<Player>members;
+
+    public Party() {
+        this.members = new ArrayList<Player>();
+    }
+
+}
